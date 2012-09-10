a59a99a35e7982a973a086705a6db8172a75972c
Manimaran Malaichamy <manim@kyyba.com>
2012-09-10 15:56:00 +0530

    added datapicker and fancy box

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,14 +14,39 @@
 //= require jquery_ujs
 //= require_tree .
 
+$(document).ready(function($) {
 
-$(document).ready(function() {
-                 $(".fancybox").fancybox({
+   $("#dateselect").datepicker();
+
+    $(".fancybox").fancybox({
            'frameWidth': 680,
           'frameHeight':495,
             'overlayShow':true,
             'hideOnContentClick':false,
             'type':'iframe'
         });
-                
-	});
+
+
+        var data = [
+    ['Heavy Industry', 12],['Retail', 9], ['Light Industry', 14],
+    ['Out of home', 16],['Commuting', 7], ['Orientation', 9]
+  ];
+
+  var plot1 = jQuery.jqplot ('chart1', [data],
+    {
+      seriesDefaults: {
+        // Make this a pie chart.
+        renderer: jQuery.jqplot.PieRenderer,
+        rendererOptions: {
+          // Put data labels on the pie slices.
+          // By default, labels show the percentage of the slice.
+          showDataLabels: true
+        }
+      },
+      legend: { show:true, location: 'e' }
+    }
+  );
+
+
+
+});
