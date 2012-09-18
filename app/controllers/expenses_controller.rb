a404cffe7b6e32e20a6f188d13579ba7a4db107c
Manimaran Malaichamy <manim@kyyba.com>
2012-09-18 22:45:53 +0530

    updated edit in expenses

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -7,6 +7,10 @@ class ExpensesController < ApplicationController
     @expenses = current_user.expenses.order('created_at desc').all
   end
 
+  def show
+    @expense = Expense.find(params[:id])
+  end
+
   def new
     @expense = Expense.new
   end
@@ -20,4 +24,24 @@ class ExpensesController < ApplicationController
       render :action => 'new'
     end
   end
+
+   def edit
+    @expense = Expense.find(params[:id])
+  end
+
+  def update
+    @expense = Expense.find(params[:id])
+    if @expense.update_attributes(params[:expense])
+      redirect_to @expense, :notice  => "Successfully updated expense."
+    else
+      render :action => 'edit'
+    end
+  end
+
+  def destroy
+    @expense = Expense.find(params[:id])
+    @expense.destroy
+    redirect_to expenses_url, :notice => "Successfully destroyed expense."
+  end
+
 end
