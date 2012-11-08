a751e8f159b6516fc4decbfd79a07b40f79aa1c6
Manimaran Malaichamy <manim@kyyba.com>
2012-11-09 00:53:03 +0530

    expense grouped by category - added to reports

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -5,6 +5,7 @@ class ReportsController < ApplicationController
     #@reports = Report.all
     @report_hash = current_user.expenses.order(:date).sum(:cost, :group => 'date')
     @total_expense = current_user.expenses.sum(:cost)
+    @category_report = current_user.expenses.order(:category_id).sum(:cost, :group => 'category_id')
     
     #@expenses = Expense.select("SUM(cost) as total_cost, MONTH(date) as month, YEAR(date) as year").group("MONTH(date), YEAR(date)") 
     #@data = @expenses.map {|expense| [expense.total_cost, expense.month, expense.year]}
